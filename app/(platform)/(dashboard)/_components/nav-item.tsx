8198533936cca4752ee8f52f1407bd6e0a3bbd73
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2024-04-27 22:19:02 -0500

    feat(workspace settings): add component OrganizationProfile of clerk

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -10,6 +10,7 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export interface Organization {
 	id: string;
@@ -104,5 +105,16 @@ export const NavItem = ({
 				))}
 			</AccordionContent>
 		</AccordionItem>
+	);
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+	return (
+		<div className='flex items-center gap-x-2 '>
+			<div className='w-10 h-10 relative shrink-0'>
+				<Skeleton className='h-full w-full absolute' />
+			</div>
+			<Skeleton className='h-10 w-full' />
+		</div>
 	);
 };
